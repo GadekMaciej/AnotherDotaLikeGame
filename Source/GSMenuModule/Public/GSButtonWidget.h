872a423c1a2ed46d9c502655cb8846c9d32b58ce
GadekMaciej <gadekom@gmail.com>
2021-09-05 19:56:47 +0200

    Prepared c++ UI classes

---
+++ Source/GSMenuModule/Public/GSButtonWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GSUserWidgetBase.h"
+#include "GSButtonWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GSMENUMODULE_API UGSButtonWidget : public UGSUserWidgetBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GSMenuModule/Public/GSServerListMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GSUserWidgetBase.h"
+#include "GSServerListMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GSMENUMODULE_API UGSServerListMenu : public UGSUserWidgetBase
+{
+	GENERATED_BODY()
+	
+};
